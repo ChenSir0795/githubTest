5c5fe467f9312e9619b68f9cb2925391fdc5c4fa
ChenSir0795 <1443432332>
2021-05-25 21:46:01 +0800

    第一次提交

---
+++ views/Abount.vue
@@ -0,0 +1,25 @@
+<template>
+<div>
+    <h2>hello vue2222</h2>
+</div>
+
+</template>
+
+<script>
+export default {
+  name:'',
+  data(){
+   return {
+
+   }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
+
+
+
+
---
+++ views/Home.vue
@@ -0,0 +1,21 @@
+<template>
+<div>
+    <h2>hello vue</h2>
+</div>
+
+</template>
+
+<script>
+export default {
+  name:'',
+  data(){
+   return {
+
+   }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
